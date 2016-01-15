245ab252e314d5174325cea93aee0ddf845b12f4
Suchaaver Chahal <suchaaverchahal@gmail.com>
2016-01-14 16:58:45 -0800

    checking

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default class extends Component {
   render() {
     return (
       <div>
-        <h1>Home Page</h1>
+        <h1>H Page</h1>
         <p>Coming soon.</p>
       </div>
     );
