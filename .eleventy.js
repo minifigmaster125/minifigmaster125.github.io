005572b2fbe6da71452e513231c90c71d5041091
Suchaaver Chahal <suchaaver.chahal@gmail.com>
2024-08-04 01:39:30 -0700

    first article pretty done, needs review

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,8 @@
+ const md = require('./utils/markdown.js')
+
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("bundle.css");
-  };
+  eleventyConfig.addPassthroughCopy("bundle.css");
+  eleventyConfig.addPassthroughCopy("assets");
+  // responsive images below does not work pls fix
+  // eleventyConfig.setLibrary('md', md)
+};
