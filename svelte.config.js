b49b8de50899f1a24ce086f20664a229362753f9
Suchaaver Chahal <suchaaverchahal@gmail.com>
2024-11-19 17:31:19 -0800

    static deploy

--- svelte.config.js
+++ svelte.config.js
@@ -15,11 +15,26 @@ const config = {
 	extensions: ['.svelte', '.md'],
 	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
+
+	// kit: {
+	// 	// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+	// 	// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+	// 	// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+	// 	adapter: adapter({
+	// 		// default options are shown. On some platforms
+	// 		// these options are set automatically — see below
+	// 		pages: 'build',
+	// 		assets: 'build',
+	// 		fallback: undefined,
+	// 		precompress: false,
+	// 		strict: true
+	// 	})
+	// }
 };
 
 export default config;
