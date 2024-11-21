b29ac4493a8697a6026a56de6c83280336da8301
Suchaaver Chahal <suchaaverchahal@gmail.com>
2024-11-20 23:25:13 -0800

    image zoom

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,13 +6,13 @@
 </script>
 
 <div class="flex min-h-screen justify-center bg-amber-100 py-8">
-	<div class="min-w-min px-8 md:w-[1024px]">
+	<div class="px-8 md:w-[1024px] min-w-min">
 		<div class="flex items-center justify-between pb-8">
 			<a href={LinkHandler('/')} class="container mx-auto py-8 font-serif">
 				<h1 class="text-2xl">such is life</h1>
 				<p class="text-sm">and other contemplation.</p>
 			</a>
-			<nav class="flex flex-col gap-x-8 font-serif sm:flex-row">
+			<nav class="flex flex-col sm:flex-row gap-x-8 font-serif">
 				<a href={LinkHandler('/blog')}>blog</a>
 				<a href={LinkHandler('/art')}>art</a>
 				<a href={LinkHandler('/about')}>about</a>
--- src/routes/art/+page.svelte
+++ src/routes/art/+page.svelte
@@ -3,8 +3,15 @@
 	import HowISteepMyTea from '$lib/assets/HowISteepMyTea.webp';
 	import WindmillsOfZaanseSchans from '$lib/assets/WindmillsOfZaanseSchans.webp';
 	import AFriendAtTheCanal from '$lib/assets/AFriendAtTheCanal.webp';
-
+	import bisonOfUtah from '$lib/assets/bisonOfUtah.webp';
+	import Mack from '$lib/assets/Mack.webp';
+	import little_flower from '$lib/assets/little_flower.jpg';
+	import momsLily from '$lib/assets/momsLily.jpg';
 	import Image from '../../components/Image.svelte'
+
+	
+	// const paths = import.meta.glob('$lib/assets/*', {eager: true})
+
 </script>
 
 <h1 class="pb-8">Original Work</h1>
@@ -19,4 +26,8 @@
 	<Image url={HowISteepMyTea} title='How I Steep My Tea' />
 	<Image url={AFriendAtTheCanal} title='A Friend at the Canal' />
 	<Image url={WindmillsOfZaanseSchans} title='Windmills of Zaanse Schans' />
+	<Image url={Mack} title='Mack' />
+	<Image url={momsLily} title="Mom's Lily" />
+	<Image url={bisonOfUtah} title='Bison in Utah' />
+	<Image url={little_flower} title='Rose' />
 </div>
