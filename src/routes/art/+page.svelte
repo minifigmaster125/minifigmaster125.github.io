4be0e7ecc706c39da956d3bc2e76e52aad9b1cbf
Suchaaver Chahal <suchaaverchahal@gmail.com>
2024-11-28 13:14:26 -0800

    LinkHandler

--- src/routes/art/+page.svelte
+++ src/routes/art/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Image from '../../components/Image.svelte';
+	import { LinkHandler } from '../../utils';
 </script>
 
 <h1 class="pb-8">Original Work</h1>
@@ -10,13 +11,13 @@
 	>.
 </p>
 <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-	<Image url={'/NationalPark.webp'} title="Del Hoge Veluwe National Park" />
-	<Image url={'/AnAfternoonBreak.webp'} title="An Afternoon Break" />
-	<Image url={'/HowISteepMyTea.webp'} title="How I Steep My Tea" />
-	<Image url={'/AFriendAtTheCanal.webp'} title="A Friend at the Canal" />
-	<Image url={'/WindmillsOfZaanseSchans.webp'} title="Windmills of Zaanse Schans" />
-	<Image url={'/Mack.webp'} title="Mack" />
-	<Image url={'/momsLily.jpg'} title="Mom's Lily" />
-	<Image url={'/bisonOfUtah.webp'} title="Bison in Utah" />
-	<Image url={'/little_flower.jpg'} title="Rose" />
+	<Image url={LinkHandler('/NationalPark.webp')} title="Del Hoge Veluwe National Park" />
+	<Image url={LinkHandler('/AnAfternoonBreak.webp')} title="An Afternoon Break" />
+	<Image url={LinkHandler('/HowISteepMyTea.webp')} title="How I Steep My Tea" />
+	<Image url={LinkHandler('/AFriendAtTheCanal.webp')} title="A Friend at the Canal" />
+	<Image url={LinkHandler('/WindmillsOfZaanseSchans.webp')} title="Windmills of Zaanse Schans" />
+	<Image url={LinkHandler('/Mack.webp')} title="Mack" />
+	<Image url={LinkHandler('/momsLily.jpg')} title="Mom's Lily" />
+	<Image url={LinkHandler('/bisonOfUtah.webp')} title="Bison in Utah" />
+	<Image url={LinkHandler('/little_flower.jpg')} title="Rose" />
 </div>
