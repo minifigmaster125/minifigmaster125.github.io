4e5a3c9394917e7600d985c9ed53cb498b76d98a
Suchaaver Chahal <suchaaverchahal@gmail.com>
2024-12-13 01:27:55 -0800

    isolation

--- src/routes/art/heroism/+page.svelte
+++ src/routes/art/heroism/+page.svelte
@@ -3,6 +3,7 @@
 	import Image from '../../../components/Image.svelte';
 </script>
 
+<div class="m-auto md:max-w-screen-md leading-relaxed">
 <h1 class="text-center">Heroism - a Dialogue</h1>
 
 <i class="mx-auto block max-w-screen-sm py-8 text-center"
@@ -37,15 +38,16 @@
 <br />
 
 <i>
-	"If the way you admired him was correct, then does that mean my admiration was wrong?" - Bakugo, during him and Izuku's first fight.
+	"If the way you admired him was correct, then does that mean my admiration was wrong?" - Bakugo,
+	during him and Izuku's first fight.
 </i>
 <p>
-	Bakugo is talking about All Might, but what neither of them understood then was nobody's admiration was correct. For Izuku, it was
-	heroism at all costs -- self-sacrifice as illustrated by his decision to take out the 0 point
-	monster in the entry exam to UA. The weight of being the heir of All for One forces a knee to
-	bend, a kind personality trait transforms into an ideal, and the Giver ... gives way. "Izuku" is
-	slowly abandoned for "Deku", in much the same way that Bruce Wayne is the mask. 
-    Self-sacrifice does not rely on others.
+	Bakugo is talking about All Might, but what neither of them understood then was nobody's
+	admiration was correct. For Izuku, it was heroism at all costs -- self-sacrifice as illustrated by
+	his decision to take out the 0 point monster in the entry exam to UA. The weight of being the heir
+	of All for One forces a knee to bend, a kind personality trait transforms into an ideal, and the
+	Giver ... gives way. "Izuku" is slowly abandoned for "Deku", in much the same way that Bruce Wayne
+	is the mask. Self-sacrifice does not rely on others.
 </p>
 <br />
 
@@ -55,14 +57,14 @@
 	strong. Pride became attached to that strength, and with pride came an immense fear of failure.
 	Perfection was the final response. Bakugo's ambition to be the #1 becomes impure, fueled by an
 	expectation followed by a self-hatred that burns incredibly hot, perhaps almost explosive. It's an
-	isolating, spiraling desperation. 
-    It's a strength that does not rely on others.
+	isolating, spiraling desperation. It's a strength that does not rely on others.
 </p>
 
 <br />
 <i>
 	"When my feeling of 'I have to beat you' is stronger than 'I have to save you' .... but the thing
-	is, my mental image of 'victory' ... is you. .. I want to beat you, the incarnation of victory!" - Izuku, to himself during their first fight.
+	is, my mental image of 'victory' ... is you. .. I want to beat you, the incarnation of victory!" -
+	Izuku, to himself during their first fight.
 </i>
 <p>
 	Every time Izuku and Bakugo have an exchange, they chip away at each other. For Izuku, it's an
@@ -71,14 +73,34 @@
 	in Deku's ideal.
 </p>
 <br />
+<p>
+	In their pursuit of their ideals, I find parallels in their power and consequently, isolation.
+	Bakugo, despite being surrounded by people that genuinely care for him, emotionally isolates
+	because dependence on them is weakness he cannot afford. Izuku, in the same situation, physically
+	isolates (in the vigilante arc) because dependence on them is a liability he cannot afford. Only
+	together can they lift each other out.
+</p>
+<br />
 <i>
 	"...but I mean it Izuku. I'm sorry for everything I did. Every action you take is in the right
-	direction. But you can barely stand now." - Bakugo, when they bring Izuku back during the vigilante arc.
+	direction. But you can barely stand now." - Bakugo, when they bring Izuku back during the
+	vigilante arc.
 </i>
 <p>
-    Bakugo was never filled with more self-conflict than when Deku acted. When Deku spoke, Bakugo grew angry. When Deku acted, Bakugo grew to doubt himself. Eventually Deku righteousness caused Bakugo's self-image to shatter, which was the only way he could be rebuilt. On the other hand, Izuku latches on to Bakugo's words. He champions "Deku", an insult Bakugo gave him. Yet in this scene, Bakugo does not say "Deku." He says "Izuku", and he says "I'm sorry". "Izuku" because nobody can be a hero before they are a person. "I'm sorry" because Izuku's heroism was perversed by a need to prove Bakugo wrong. "I'm sorry" because it's the hardest thing for a man with such pride to say. "I'm sorry" because Izuku is sorry as well.
+	Bakugo was never filled with more self-conflict than when Deku acted. When Deku spoke, Bakugo grew
+	angry. When Deku acted, Bakugo grew to doubt himself. Eventually Deku's righteousness caused
+	Bakugo's self-image to shatter, which was the only way he could be rebuilt. On the other hand,
+	Izuku latches on to Bakugo's words. He champions "Deku", an insult Bakugo gave him. Yet in this
+	scene, Bakugo does not say "Deku." He says "Izuku", and he says "I'm sorry".
+	<strong>"Izuku"</strong> because nobody can be a hero before they are a person. <strong>"I'm sorry"</strong> because
+	Izuku's heroism was perversed by a need to prove Bakugo wrong. <strong>"I'm sorry"</strong> because it's the most difficult
+	and authentic thing for a man shaped by pride to say. <strong>"I'm sorry"</strong> because Izuku is sorry as well.
 </p>
-<br>
-<p> 
-    Perhaps the paintings exist as a dialogue between the two. "Heavy is the Crown" as Deku acknowledging the isolation and crushing weight that were suffocating Bakugo. "Is This the Strength You Sought?" as Bakugo asks Deku if the Hero is worth the price of the man. 
+<br />
+<p>
+	Perhaps the paintings exist as a dialogue between the two. "Heavy is the Crown" as Deku
+	acknowledging the isolation and crushing weight that were suffocating Bakugo. "Is This the
+	Strength You Sought?" as Bakugo asks Deku if the Hero is worth the price of the man.
 </p>
+
+</div>
