77639b9fcc31298129a49b5295eaf64707420555
Suchaaver Chahal <suchaaverchahal@gmail.com>
2025-01-05 19:35:25 -0800

    i don't need it

--- src/routes/art/[slug].bk/+page.ts
+++
@@ -1,9 +0,0 @@
-
-import type { PageLoad } from './$types';
-import products from '../../../products.json';
-
-export const load: PageLoad = ({ params }) => {
-    const product = products.find(p => p.slug == params.slug)
-    console.log(product)
-	return product
-};
