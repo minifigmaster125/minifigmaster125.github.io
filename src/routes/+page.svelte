3532e4eeb7e842d1ba86e37981ea2c3acc6e8621
Suchaaver Chahal <suchaaverchahal@gmail.com>
2026-06-12 21:56:00 -0400

    update main

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,15 @@
 <script>
-	import { LinkHandler } from "../utils";
-
+	import { LinkHandler } from '../utils';
 </script>
 
-<p class="pb-4"> Hi! I'm Suchaaver. I have a penchant for the pithy. </p>
-<p class="pb-4"> Here you'll find various thoughts on software engineering, art, and other topics as I engage in creativity and curiosity. You can also see some of my artwork. With any luck, the stay will be worth your while. </p> 
-<p class="pb-4"> If you'd like, you can get all of the above to your inbox by <a  class="text-amber-600 cursor-pointer" href={LinkHandler("/subscribe")}>subscribing</a>.</p> 
+<p class="pb-4">Hi! I'm Suchaaver. I have a penchant for the pithy.</p>
+<p class="pb-4">
+	Here you'll find various thoughts on software engineering, art, and game development as I engage
+	in creativity and curiosity.
+</p>
+<p class="pb-4">
+	You can get said thoughts in your inbox by <a
+		class="cursor-pointer text-amber-600"
+		href={LinkHandler('/subscribe')}>subscribing</a
+	>.
+</p>
